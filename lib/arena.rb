470c572d4509e49da3a9d293eb757dbe07fa17be
castrolol <lu4n.k4str0@gmail.com>
2015-01-18 13:40:22 -0300

    Mudanças em Personagem e Arena
    Removida a responsabilidade de iniciar com posição do Personagem,
    atribuida a Arena
    Atribudo a Arena a responsabilidade de gerenciar ataques à arena
    Incluido uma verificação junto ao metodo "estar na arena" da arena,
    possibilitando apenas personagens
    que  foram inseridos na arena
    
    criado testes para os itens acima

--- lib/arena.rb
+++ lib/arena.rb
@@ -13,24 +13,26 @@ class Arena
 		(distancia(personagem_1,personagem_2) < maior_distancia)
 	end
 
-	def distancia(personagem_1,personagem_2)
+	def distancia(personagem_1, personagem_2)
 		a = (personagem_1.x - personagem_2.x)**2
 		b = (personagem_1.y - personagem_2.y)**2
 		Math.sqrt(a+b)
 	end
 
 	def add_personagem(personagem, x, y)
-		personagens.append(personagem)
+		@personagens.push(personagem)
 		personagem.x = x
 		personagem.y = y
+		personagem.arena = self
 	end
 
 	def dentro_arena?(x, y)
-		x.between? 1, @width and y.between? 1, @height
+		x.between? 1, @width and y.between? 1, @height	
 	end
 
 	def na_arena?(personagem)
-		self.dentro_arena? personagem.x, personagem.y
+		nos_limites = self.dentro_arena? personagem.x, personagem.y
+		@personagens.include?(personagem) && nos_limites
 	end
 
 	def mover(personagem, x, y)
@@ -42,5 +44,23 @@ class Arena
 
 		false
 	end
+ 
+	def pode_atacar(atacante, atacado)
+		distancia = self.distancia(atacante, atacado)		
+		if atacante.arma.distancia >= distancia then 
+			return true
+		end	
+		false
+	end
 
+	def atacar(atacante, atacado)
+		if atacante.arma == nil then
+		 raise "atacante necessita de uma arma"
+		end
+ 
+		if  self.pode_atacar(atacante, atacado) then
+			atacado.hp -= atacante.arma.dano
+		end
+	end
 end
+ 
